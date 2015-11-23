4c400062f33670f4ac9b231071125df30462930c
tony-zhou <tony.3.4.1900@gmail.com>
2015-11-23 13:52:06 +0800

    Python script for generating the error log after issues happen.

---
+++ gen_error_log/main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import os
+import time
+from utils.commonfun import Common
+
+
+__author__ = 'tony'
+
+
+class Remedy(object):
+
+    def __init__(self):
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.error_type = ["data_app_anr", "data_app_crash"]
+        self.device_id = ""
+
+    def get_log(self):
+        self.device_id = Common.gen_devices_id(single=True)[0]
+        device_info = '_'.join(Common.gen_device_info(self.device_id)).replace(' ', '_')
+        log_path = os.path.join(self.path, *['Log', device_info])
+        for item in self.error_type:
+            error_list = self.gen_error_list(item)
+            Common.print_log("start to get %s log" % item.split('_')[2])
+            if bool(error_list):
+                item_path = os.path.join(log_path, ''.join(['Log_', item.split('_')[2]]))
+                latest_path = os.path.join(item_path, 'Latest')
+                Common.confirm_path(item_path, update=False)
+                Common.confirm_path(latest_path, update=True)
+                self.write_log(latest_path, error_list[0])
+                if self.need_to_dump_log(error_list[0]):
+                    self.dump_log(latest_path, error_list[0], item)
+                if len(error_list) > 1:
+                    for e in error_list[1:]:
+                        self.write_log(item_path, e)
+                print(u"{0:s} logs are successfully exported !".format(
+                    ''.join([item.split('_')[2][0].upper(), item.split('_')[2][1:].lower()])))
+            else:
+                Common.print_log("there is no valid %s log !" % item.split('_')[2])
+
+    def write_log(self, path, error):
+        file_name = error.replace(':', '-').replace(' ', '_')
+        log_file = os.path.join(path, ''.join([file_name, '.txt']))
+        if not os.path.exists(log_file):
+            with open(log_file, mode='w', encoding='utf-8') as file:
+                cmd = u'dumpsys dropbox --print {0:s}'.format(error)
+                log_info = Common.adb_shell(self.device_id, cmd)
+                file.write(log_info[0])
+                file.close()
+
+    def dump_log(self, path, error, item):
+        file_name = ''.join([error.replace(':', '-').replace(' ', '_'), 'logcat.txt'])
+        log_file = os.path.join(path, file_name)
+        if not os.path.exists(log_file):
+            with open(log_file, mode='w', encoding='utf-8') as file:
+                cmd = 'logcat -v time -d'
+                log_info = Common.adb(cmd, self.device_id)
+                file.write(log_info[0])
+                file.close()
+            if item.split('_')[2] == 'anr':
+                cmd = u'pull {0:s} {1:s}'.format('/data/anr/traces.txt', path)
+                Common.adb(self.device_id, cmd)
+
+    def gen_error_list(self, error_type):
+        error_list = []
+        cmd = 'dumpsys dropbox'
+        response = Common.adb_shell(cmd, self.device_id)[0]
+        if not bool(response):
+            return False
+        else:
+            result = [x for x in response.strip().split('\n') if error_type in x]
+            if result == ['']:
+                return False
+            else:
+                for item in result:
+                    if 'contents lost' not in item:
+                        time_stamp = ' '.join([item.split(" ")[0], item.split(" ")[1]])
+                        error_list.append(time_stamp)
+            error_list.sort(reverse=True)
+            return error_list
+
+    def need_to_dump_log(self, time_stamp):
+        log_unix_time = int(time.mktime(time.strptime(time_stamp, '%Y-%m-%d %H:%M:%S')))
+        cmd = 'date +%s'
+        current_unix_time = Common.adb_shell(cmd, self.device_id)
+        current_unix_time = int(current_unix_time[0])
+        if (current_unix_time - log_unix_time) in range(180):
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    log = Remedy()
+    log.get_log()
---
+++ utils/commonfun.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import os
+import time
+import shutil
+import subprocess
+
+
+__author__ = 'tony'
+
+
+class Common(object):
+
+    @staticmethod
+    def gen_devices_id(single=False):
+        if not Common.check_connection():
+            Common.print_log('there is no connected device')
+            exit()
+        Common.print_log("start to get devices id")
+        total_devices_id = []
+        valid = False
+        while not valid:
+            devices_info = Common.adb('devices')[0].split('\n')
+            for detail_info in devices_info[::-1]:
+                if '' == detail_info or 'List' in detail_info:
+                    continue
+                else:
+                    if 'offline' in detail_info:
+                        Common.print_log(''.join(["device ", detail_info.split('\t')[0], "offline"]))
+                        input("please press Enter to retry after check")
+                        valid = False
+                        break
+                    elif 'unauthorized' in detail_info:
+                        Common.print_log(''.join(["device", detail_info.split('\t')[0], "unauthorized"]))
+                        input("please press Enter to retry after check")
+                        valid = False
+                        break
+                    elif 'device' in detail_info:
+                        total_devices_id.append(detail_info.split('\t')[0])
+                        valid = True
+        devices_number = len(total_devices_id)
+        if single and devices_number > 1:
+            Common.print_log('there are multi devices connected')
+            exit()
+        else:
+            return total_devices_id
+
+    @staticmethod
+    def gen_device_info(device_id):
+        model = ''
+        brand = ''
+        Common.print_log("start to get device info")
+        cmd = 'cat /system/build.prop'
+        device_info = Common.adb_shell(cmd, device_id)[0].strip().split('\n')
+        for detail_info in device_info:
+            if 'product.brand' in detail_info:
+                brand = detail_info.strip()[detail_info.find('=') + 1:]
+            elif 'product.model' in detail_info:
+                model = detail_info.strip()[detail_info.find('=') + 1:]
+        if not bool(model):
+            model = device_id
+        print("device info is got as %s %s" % (brand.upper(), model.upper()))
+        return brand.upper(), model.upper()
+
+    @staticmethod
+    def check_connection(device_id=""):
+        if bool(device_id):
+            devices_info = Common.adb('get-state', device_id)[0]
+            a = type(devices_info)
+            if 'device' in devices_info:
+                return True
+            else:
+                return False
+        else:
+            devices_info = Common.adb('devices')[0].split()
+            if 'device' in devices_info:
+                return True
+            else:
+                return False
+
+    @staticmethod
+    def confirm_connection(device_id):
+        valid = False
+        while not valid:
+            time.sleep(1)
+            devices_info = Common.adb('devices')[0].split('\n')
+            for check_valid in devices_info[::-1]:
+                if device_id in check_valid and 'device' in check_valid:
+                    valid = True
+                    break
+            if not valid:
+                input(u"device {0:s} is invalid, press Enter after check to retry".format(device_id))
+        return True
+
+    @staticmethod
+    def adb(cmd, device_id=''):
+        cmd = u"adb -s {0:s} {1:s}".format(device_id, cmd) if bool(device_id) else u"adb {0:s}".format(cmd)
+        f = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        feedback = f.communicate()
+        return feedback
+
+    @staticmethod
+    def adb_shell(cmd, device_id=''):
+        cmd = u"adb -s {0:s} shell {1:s}".format(device_id, cmd) if bool(device_id) else u"adb shell {0:s}".format(cmd)
+        f = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        feedback = f.communicate()
+        return feedback
+
+    @staticmethod
+    def command(cmd):
+        f = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        feedback = f.communicate()
+        return feedback
+
+    @staticmethod
+    def print_log(info, tag=''):
+        time_stamp = time.strftime(r'%Y-%m-%d %H:%M:%S', time.localtime())
+        if bool(tag):
+            print(''.join([time_stamp, ' '*8, tag, ' '*8, info]))
+        else:
+            print(''.join([time_stamp, ' '*8, info]))
+
+    @staticmethod
+    def save_log(log, info, tag):
+        with open(log, 'a+', encoding='utf-8') as file:
+            time_stamp = time.strftime(r'%Y-%m-%d %H:%M:%S', time.localtime())
+            if bool(tag):
+                file.write(''.join([time_stamp, ' '*8, tag, ' '*8, info]))
+            else:
+                file.write(''.join([time_stamp, ' '*8, info]))
+            file.close()
+
+    @staticmethod
+    def confirm_path(path, update=False):
+        if os.path.exists(path):
+            if update:
+                try:
+                    shutil.rmtree(path)
+                except IOError:
+                    Common.print_log('delete files in {0:s} fail, please close it and retry'.format(path))
+                    exit()
+        else:
+            if os.path.exists(os.path.basename(path)):
+                os.mkdir(path)
+            else:
+                os.makedirs(path)
+
+
+if __name__ == "__main__":
+    pass
